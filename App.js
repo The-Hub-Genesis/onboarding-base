8f1f3923bf221787c4cf55a27bdd4ee3adac5e45
knee-rel <nirelmarieibarra@gmail.com>
2021-05-12 20:49:23 +0800

    added async storage to onboarding screens

--- App.js
+++ App.js
@@ -1,13 +1,32 @@
 import { StatusBar } from "expo-status-bar";
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { StyleSheet, Text, View } from "react-native";
+import AsyncStorage from '@react-native-async-storage/async-storage';
+
 import LoginScreen from "./screens/LoginScreen";
-import Navigator from './navigate/NavigationStack'
+import Navigator from "./navigate/NavigationStack";
 
 export default function App() {
-  return (
-    <Navigator />
-  );
+  const [isFirstLaunch, setIsFirstLaunch] = React.useState(null);
+
+  useEffect(() => {
+    AsyncStorage.getItem("alreadyLaunched").then((value) => {
+      if (value == null) {
+        AsyncStorage.setItem("alreadyLaunched", "true");
+        setIsFirstLaunch(true);
+      } else {
+        setIsFirstLaunch(false);
+      }
+    });
+  }, []);
+
+  if (setIsFirstLaunch === null) {
+    return null;
+  } else if (isFirstLaunch === true) {
+    return <Navigator />;
+  } else {
+    return <LoginScreen />;
+  } 
 }
 
 const styles = StyleSheet.create({
